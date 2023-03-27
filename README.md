fe529022904192cc043425e707ff8eab3828240c
Vítor F. Nery <vitorfelipenery@gmail.com>
2023-03-27 11:44:29 -0300

    readme add

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello World
+
+Simple "Hello World" page using React, Vite and Tailwind CSS.
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
     <div className="h-screen w-screen bg-black flex justify-center items-center">
-      <h1 className="text-8xl font-sans text-white">Hello, World!</h1>
+      <h1 className="text-5xl lg:text-7xl  xl:text-9xl font-sans text-white">
+        Hello, World!
+      </h1>
     </div>
   );
 };
