28f0f1567a62bdde95367f9a1aff866fdacd7c63
Vítor F. Nery <vitorfelipenery@gmail.com>
2023-03-27 11:36:48 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="h-screen w-screen bg-black flex justify-center items-center">
+      <h1 className="text-8xl font-sans text-white">Hello, World!</h1>
+    </div>
+  );
+};
+
+export default App;
